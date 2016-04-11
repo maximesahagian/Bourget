c4dd6dd062ad29d4f6ab61c61772073bdb2d901d
maximesahagian <maxime.sahagian06@gmail.com>
2016-04-11 15:12:45 +0200

    French

--- App/resources/views/welcome.blade.php
+++ App/resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bienvenue</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Votre page pour l'application
                 </div>
             </div>
         </div>
