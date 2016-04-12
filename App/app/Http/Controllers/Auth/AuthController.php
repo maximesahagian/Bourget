9d420d4bed4f177c3d042f80890a0fd978c29e26
Samax06 <maxime.sahagian06@gmail.com>
2016-04-12 17:23:23 +0200

    Adding pseudo input

--- App/app/Http/Controllers/Auth/AuthController.php
+++ App/app/Http/Controllers/Auth/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|max:255',
+            'pseudo' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -65,6 +66,7 @@ class AuthController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'pseudo' => $data['pseudo'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
